28b79960f0096bec3a351c64081f33ca027e2698
kingofgreatnezzz <kingofgreatnezzz@gmail.com>
2024-07-04 11:55:41 -0700

    art works added

--- src/content_option.js
+++ src/content_option.js
@@ -1,161 +1,264 @@
-const logotext = "{OfodileArtPriest}";
+import art1 from "../src/assets/images/arts/art1.png";
+import art2 from "../src/assets/images/arts/art2.png";
+import art3 from "../src/assets/images/arts/art3.png";
+import art4 from "../src/assets/images/arts/art5.png";
+import art5 from "../src/assets/images/arts/art6.png";
+import art6 from "../src/assets/images/arts/art4.png";
+import art7 from "../src/assets/images/arts/art7.png";
+import art8 from "../src/assets/images/arts/art8.png";
+import art9 from "../src/assets/images/arts/art9.png";
+import art10 from "../src/assets/images/arts/art10.png";
+import art11 from "../src/assets/images/arts/art11.png";
+import art12 from "../src/assets/images/arts/art12.png";
+import art13 from "../src/assets/images/arts/art13.png";
+import art14 from "../src/assets/images/arts/art14.png";
+import art15 from "../src/assets/images/arts/art15.png";
+import art16 from "../src/assets/images/arts/art16.png";
+import art17 from "../src/assets/images/arts/art17.png";
+import art18 from "../src/assets/images/arts/art18.png";
+import Logo from "../src/assets/images/arts/artpriestLogo.png";
+
+
+const images = [
+  art1,
+  art2,
+  art3,
+  art5,
+  art6,
+  art4,
+  art7,
+  art8,
+  art9,
+  art10,
+  art11,
+  art12,
+  art13,
+  art14,
+  art15,
+  art16,
+  art17,
+  art18,
+  Logo,
+];
+
+export default images;
+
+const logotext = "OfodileArtPriest";
 const meta = {
-    title: "{ofodile Art Priest}",
-    description: "My Art Portfolio",
+  title: "{ofodile Art Priest}",
+  description: "My Art Portfolio",
 };
 
 const introdata = {
-    title: "Hi, I'm Ofodile Art Priest",
-    animated: {
-        first: "Artist",
-        second: "Visionary",
-        third: "Business",
-    },
-    description: "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
-    your_img_url: "https://i.ibb.co/wRjcnGL/me.jpg",
+  title: "Hi, I'm Ofodile ArtPriest",
+  animated: {
+    first: "Artist",
+    second: "Visionary",
+    third: "Business",
+  },
+  description:
+    "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
+  your_img_url: images[18],
 };
 
 const dataabout = {
-    title: "Who am I?",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  title: "Who am I?",
+  aboutme:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 };
 
-const worktimeline = [{
-        jobtitle: "Example 1",
-        where: "Google",
-        date: "2023",
-    },
-    {
-        jobtitle: "Example 2",
-        where: "Micosoft",
-        date: "2022",
-    },
-    {
-        jobtitle: "Example 3",
-        where: "Meta",
-        date: "2021",
-    },
+const worktimeline = [
+  {
+    jobtitle: "Example 1",
+    where: "Google",
+    date: "2023",
+  },
+  {
+    jobtitle: "Example 2",
+    where: "Micosoft",
+    date: "2022",
+  },
+  {
+    jobtitle: "Example 3",
+    where: "Meta",
+    date: "2021",
+  },
 ];
 
-const skills = [{
-        name: "Python",
-    },
-    
-    {
-        name: "C",
-    },
-    
-    {
-        name: "Java",
-    },
-    
-    {
-        name: "Bash",
-    },
+const skills = [
+  {
+    name: "Python",
+  },
 
-    {
-        name: "Powershell",
-    },
+  {
+    name: "C",
+  },
 
-    {
-        name: "Reverse Engineering",
-    },
+  {
+    name: "Java",
+  },
 
-    {
-        name: "Web-Exploitation",
-    },
+  {
+    name: "Bash",
+  },
 
-    {
-        name: "Network Exploitation",
-    },
+  {
+    name: "Powershell",
+  },
+
+  {
+    name: "Reverse Engineering",
+  },
+
+  {
+    name: "Web-Exploitation",
+  },
+
+  {
+    name: "Network Exploitation",
+  },
 ];
 
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+const dataportfolio = [
+  {
+    img: images[0],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[1],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[2],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[3],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[4],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[5],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[6],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[7],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[8],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[9],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[10],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[11],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[12],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[13],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[14],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[15],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[16],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[17],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: images[18],
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "test@test.com",
-    description: "Looking forward to hearing from you!",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "SERVICE_ID",
-    YOUR_TEMPLATE_ID: "TEMPLATE_ID",
-    YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
+  YOUR_EMAIL: "test@test.com",
+  description: "Looking forward to hearing from you!",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "SERVICE_ID",
+  YOUR_TEMPLATE_ID: "TEMPLATE_ID",
+  YOUR_USER_ID: "USER_ID (PUBLIC_KEY)",
 };
 
 const socialprofils = {
-    github: "https://github.com/",
-    linkedin: "https://linkedin.com/",
+  github: "https://github.com/",
+  linkedin: "https://linkedin.com/",
 };
 
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
